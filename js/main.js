0839cdf546a6c55361dc8569f86361a7a27aa3b8
Наталья <nataldenal89@gmail.com>
2020-02-13 22:48:26 +0500

    финал №2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 'use strict';
 
-var IMG_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8];
+var QUANTITY_ADS = 8;
+var WIDTH_PIN = 65;
 var AD_TITLES = ['Двухкомнатная квартира с видом на океан!', 'Трехкомнатная квартира для семейного отдыха', 'Роскошные апартаменты'];
 var AD_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var ADS_CHECK = ['12:00', '13:00', '14:00'];
 var AD_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var AD_DESCRIPTIONS = ['Все необходимые удобства и красивый вид из окна.', 'Удобное месторасположение, рядом исторический музей и ТРЦ.'];
 var AD_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var QUANTITY_ADS = 8;
-var WIDTH_PIN = 65;
 
+var adAdress = '350, 340';
 var ads = [];
 var map = document.querySelector('.map');
 var mapPins = map.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin')
-.content;
+.content
+.querySelector('.map__pin');
 
 map.classList.remove('map--faded');
 
@@ -26,33 +27,37 @@ var getRandomNumber = function (adArr) {
 var getRandomNumberMinMax = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
-var featLen = getRandomNumberMinMax(1, 6);
-var photLen = getRandomNumberMinMax(1, 3);
 
-AD_FEATURES.length = featLen;
-AD_PHOTOS.length = photLen;
+var getRandomLength = function (arr) {
+  var arrLength = [];
+  for(var t = 0; t < getRandomNumber(arr); t++) {
+    arrLength[t] = arr[t];
+  }
 
-var arrAdsСreation = function () {
+  return arrLength;
+};
+
+var getAds = function () {
   for (var i = 0; i < QUANTITY_ADS; i++) {
     ads[i] = {
       author: {
-        avatar: 'img/avatars/user0' + IMG_NUMBERS[i] + '.png'
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
       offer: {
         title: AD_TITLES[getRandomNumber(AD_TITLES)],
-        address: '350, 350',
+        address: adAdress,
         price: getRandomNumberMinMax(2000, 6000),
         type: AD_TYPES[getRandomNumber(AD_TYPES)],
         rooms: getRandomNumberMinMax(1, 5),
         guests: getRandomNumberMinMax(1, 8),
         checkin: ADS_CHECK[getRandomNumber(ADS_CHECK)],
         checkout: ADS_CHECK[getRandomNumber(ADS_CHECK)],
-        features: AD_FEATURES,
+        features: getRandomLength(AD_FEATURES),
         description: AD_DESCRIPTIONS[getRandomNumber(AD_DESCRIPTIONS)],
-        photos: AD_PHOTOS,
+        photos: getRandomLength(AD_PHOTOS),
       },
       location: {
-        x: getRandomNumberMinMax(1, map.offsetWidth),
+        x: getRandomNumberMinMax(0, map.offsetWidth),
         y: getRandomNumberMinMax(130, 630),
       }
     };
@@ -60,12 +65,12 @@ var arrAdsСreation = function () {
   return ads[i];
 };
 
-arrAdsСreation();
+getAds();
 
 var renderPin = function (ad) {
   var pinElement = pinTemplate.cloneNode(true);
 
-  pinElement.querySelector('.map__pin').style = 'left: ' + (ad.location.x - WIDTH_PIN / 2) + 'px; top: ' + (ad.location.y - WIDTH_PIN) + 'px';
+  pinElement.style = 'left: ' + (ad.location.x - WIDTH_PIN / 2) + 'px; top: ' + (ad.location.y - WIDTH_PIN) + 'px';
   pinElement.querySelector('.map__pin img').src = ad.author.avatar;
   pinElement.querySelector('.map__pin img').alt = ad.offer.title;
 
